10e475ee7f8b834e01e126f54ecacee37120ed2c
Nazrul Hisham <nazrulhisham@bespoke.com.my>
2018-07-12 15:24:38 +0800

    Resolved Popper.js error

--- app/main.js
+++ app/main.js
@@ -5,11 +5,18 @@
         'plugins': '../lib/durandal/js/plugins',
         'transitions': '../lib/durandal/js/transitions',
         'knockout': '../lib/knockout/knockout-3.4.0',
+        'bootstrap': 'https://stackpath.bootstrapcdn.com/bootstrap/4.1.1/js/bootstrap.bundle.min',
         'jquery': '../lib/jquery/jquery-3.1.1'
+    },
+    shim: {
+        'bootstrap': {
+            deps: ['jquery'],
+            exports: 'jQuery'
+        }
     }
 });
 
-define(['durandal/system', 'durandal/app', 'durandal/viewLocator'], function (system, app, viewLocator) {
+define(['durandal/system', 'durandal/app', 'durandal/viewLocator', 'bootstrap'], function (system, app, viewLocator) {
     //>>excludeStart("build", true);
     system.debug(true);
     //>>excludeEnd("build");
@@ -22,11 +29,7 @@ define(['durandal/system', 'durandal/app', 'durandal/viewLocator'], function (sy
     });
 
     app.start().then(function () {
-        //Replace 'viewmodels' in the moduleId with 'views' to locate the view.
-        //Look for partial views in a 'views' folder in the root.
         viewLocator.useConvention();
-
-        //Show the app by setting the root view model for our application with a transition.
         app.setRoot('viewmodels/shell', 'entrance');
     });
 });
